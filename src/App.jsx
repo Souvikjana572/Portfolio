dfedfa4219cea555294713d786ff554d42bb120c
Souvikjana572 <souvikj572@gmail.com>
2026-02-02 20:44:00 +0530

    config app

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Home from "./sections/Home";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
-import Experience from "./sections/Experience";
-import Testimonials from "./sections/Testimonials";
+import Experience from "./sections/Experience";;
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 import MusicPlayer from "./components/MusicPlayer";
@@ -31,7 +30,6 @@ export default function App() {
       <Skills />
       <Projects />
       <Experience />
-      <Testimonials />
       <Contact />
       <Footer />
     </div>
